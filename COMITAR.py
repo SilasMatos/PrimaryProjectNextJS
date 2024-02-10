02549e967cba1675cf2e01475e8e172ee01b53a2
Silas Matos <silas.progamacao@gmail.com>
2024-02-10 13:50:42 -0300

    Alteracao na section

--- COMITAR.py
+++ COMITAR.py
@@ -5,11 +5,10 @@ def simulate_git_commit_push():
 
     subprocess.run(["git", "add", "."])
 
-
     commit_message = "Alteracao na section"
     subprocess.run(["git", "commit", "-m", commit_message])
 
-    subprocess.run(["git", "push"])
+    subprocess.run(["git", "push", "origin", "master"])
 
     print("Commit realizado com sucesso!!")
 
